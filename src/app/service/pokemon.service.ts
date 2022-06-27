9d9ee2b0dfcb0a85040ba49e06455a6b00645768
Andre Cordeiro <andre.aci@dpf.gov.br>
2022-06-26 22:43:23 -0300

    Adaptação do component para receber rest

--- src/app/service/pokemon.service.ts
+++ src/app/service/pokemon.service.ts
@@ -8,15 +8,19 @@ import { Observable } from 'rxjs';
 })
 export class PokemonService {
    private readonly API = "http://localhost:3000/pokemons"
+   private readonly APIPOKEMON = "http://localhost:8080/pokemon"
   constructor(private $http:HttpClient) { }
 
+  todosPokemons():Observable<Pokemon[]>{
+  return this.$http.get<Pokemon[]>(`${this.APIPOKEMON}/all`)
+  }
 
 
-listarPokemons(){
-  return this.$http.get<Pokemon[]>(`${this.API}`)
+  listarPokemons(){
+    return this.$http.get<Pokemon[]>(`${this.API}`);
 
-}
-pokemonPorId(id:any):Observable<Pokemon>{
-  return this.$http.get<Pokemon>(`${this.API}/${id}`)
-}
+  }
+  pokemonPorId(id:any):Observable<Pokemon>{
+    return this.$http.get<Pokemon>(`${this.APIPOKEMON}/${id}`);
+  }
 }
--- src/app/vizualizar-pokemon/vizualizar-pokemon.component.ts
+++ src/app/vizualizar-pokemon/vizualizar-pokemon.component.ts
@@ -12,90 +12,72 @@ import { ActivatedRoute } from '@angular/router';
 export class VizualizarPokemonComponent implements OnInit {
 
 
-  constructor(private activated:ActivatedRoute,private pokServ:PokemonService) {}
-nomePokemon:any
-ataquePokemon:any
-defesaPokemon:any
-imagePokemon:any
-tipoPokemon:any
-fraqPokemon:any
-grid:any
-id:any
-pokemon :Pokemon
+  constructor(private activated:ActivatedRoute,
+    private pokServ:PokemonService) {}
+
+  nomePokemon:any
+  ataquePokemon:any
+  defesaPokemon:any
+  imagePokemon:any
+  tipoPokemon:any
+  fraqPokemon:any
+  grid:any
+  id:any
+  pokemon :Pokemon
+
   ngOnInit(): void {
-
-// console.log(this.nomePokemon);
-this.nomePokemon = document.querySelector('.nomep')
-this.ataquePokemon = document.querySelector('.ataquep')
-this.defesaPokemon = document.querySelector('.defesap')
-this.imagePokemon = document.querySelector('.imagep')
-this.tipoPokemon = document.querySelector('.tipop')
-this.fraqPokemon = document.querySelector('.fraqp')
-this.grid =document.querySelector('.central')
-// console.log(this.nome);
-// console.log(this.getNome);
+    this.parametrosDaRotaLabels();
+    this.animation();
+  }
 
 
-this.activated.params.subscribe(parametros => {
-  if (parametros['id']) {
-    this.id = parametros['id'];
-    this.pokServ.pokemonPorId(this.id).subscribe(pokemon => {
-      this.pokemon = pokemon;
-      this.nomePokemon.innerHTML=pokemon.nome
-      this.ataquePokemon.innerHTML="Ataque: "+pokemon.ataque
-      this.defesaPokemon.innerHTML="Defesa: "+pokemon.defesa
-      this.tipoPokemon.innerHTML="Tipo: : "+pokemon.tipo
-      this.fraqPokemon.innerHTML="Fraqueza: "+pokemon.fraqueza
-      this.imagePokemon.src=pokemon.image
-      console.log(this.pokemon.nome);
+  animation(){
+  //Animação imagem pokemon
+  this.imagePokemon.style.transform="translateX(220px)"
+  this.imagePokemon.style.opacity='0'
+  //Animação nome pokemon
+  this.nomePokemon.style.transform="translateX(-220px)"
+  this.nomePokemon.style.opacity='0'
+  //Animação da Grid
+  this.grid.style.transform="translateY(-220px)"
+  this.grid.style.opacity='0'
+  setTimeout(() => {
+    this.imagePokemon.style.transition='all 1s ease-in-out'
+    this.imagePokemon.style.opacity='1'
+    this.imagePokemon.style.transform="translateX(0px)"
 
+    this.nomePokemon.style.transition='all 1s ease-in-out'
+    this.nomePokemon.style.opacity='1'
+    this.nomePokemon.style.transform="translateX(0px)"
+
+    this.grid.style.transition='all 1s ease-in-out'
+    this.grid.style.opacity='1'
+    this.grid.style.transform="translateY(0px)"
+  }, 200);
+  }
+  parametrosDaRotaLabels(){
+    this.nomePokemon = document.querySelector('.nomep');
+    this.ataquePokemon = document.querySelector('.ataquep');
+    this.defesaPokemon = document.querySelector('.defesap');
+    this.imagePokemon = document.querySelector('.imagep');
+    this.tipoPokemon = document.querySelector('.tipop');
+    this.fraqPokemon = document.querySelector('.fraqp');
+    this.grid =document.querySelector('.central');
+    this.activated.params.subscribe(parametros => {
+
+      if (parametros['id']) {
+        this.id = parametros['id'];
+        this.pokServ.pokemonPorId(this.id).subscribe(pokemon => {
+          this.pokemon = pokemon;
+          this.nomePokemon.innerHTML=pokemon.nome
+          this.ataquePokemon.innerHTML="Ataque: "+pokemon.ataque
+          this.defesaPokemon.innerHTML="Defesa: "+pokemon.defesa
+          this.tipoPokemon.innerHTML="Tipo: "+pokemon.tipo
+          this.fraqPokemon.innerHTML="Fraqueza: "+pokemon.fraqueza
+          this.imagePokemon.src=pokemon.image
+
+        });
+      }
     });
   }
-});
-//Animação imagem pokemon
-this.imagePokemon.style.transform="translateX(220px)"
-this.imagePokemon.style.opacity='0'
-//Animação nome pokemon
-this.nomePokemon.style.transform="translateX(-220px)"
-this.nomePokemon.style.opacity='0'
-//Animação da Grid
-this.grid.style.transform="translateY(-220px)"
-this.grid.style.opacity='0'
-setTimeout(() => {
-  this.imagePokemon.style.transition='all 1s ease-in-out'
-  this.imagePokemon.style.opacity='1'
-  this.imagePokemon.style.transform="translateX(0px)"
-
-  this.nomePokemon.style.transition='all 1s ease-in-out'
-  this.nomePokemon.style.opacity='1'
-  this.nomePokemon.style.transform="translateX(0px)"
-
-  this.grid.style.transition='all 1s ease-in-out'
-  this.grid.style.opacity='1'
-  this.grid.style.transform="translateY(0px)"
-
-
-}, 200);
-
-  }
-
-//   nome:string;
-//   ataque:any;
-//   image:any;
-//   defesa:number;
-
-//   mostrarPokemon(pokes:any){
-
-//   this.nome = pokes.nome
-//   this.ataque=pokes.ataque
-
-//   console.log(pokes.nome+", "+ this.nome );
-
-//   }
-
-
-// public get getNome() : string {
-//   return this.nome
-// }
-
 }
